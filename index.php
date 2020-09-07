87d5b790fdbf680c0091166b686b1e75546adb2c
epicurean21 <jm_company@naver.com>
2020-09-07 17:19:42 +0000

    Jeremih indexphp repush

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ date_default_timezone_set('Asia/Seoul');
 ini_set('default$Longtitude_charset', 'utf8mb4');
 
 //에러출력하게 하는 코드
-error_reporting(E_ALL); ini_set("display_errors", 1);
-
+//error_reporting(E_ALL); ini_set("display_errors", 1);
+//tmp
 //Main Server API
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
     /* ******************   Test   ****************** */
